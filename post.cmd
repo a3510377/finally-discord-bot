f7006ff92ca1ba15b68359b2768a58a0ac400860
a3510377 <a102009102009@gmail.com>
2021-10-23 16:40:23 +0800

    set

--- post.cmd
+++ post.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-npm run build:pro && cd dist && git init && git add . && git commit -m "deploy" && git push git@github.com:a3510377/finally-discord-bot.git master:gh-pages
+npm run build:pro && cd dist && git init && git add . && git commit -m "deploy" && git push -f git@github.com:a3510377/finally-discord-bot.git master:gh-pages
